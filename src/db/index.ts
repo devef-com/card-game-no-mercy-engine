a22bea9a5c5348f4b87dd620ebd00de81f3b0b69
SergioJoonik <smorales@joonik.com>
2025-12-19 15:41:56 -0500

    clean up project

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,20 @@
+import "server-only";
 import { drizzle } from "drizzle-orm/libsql";
+
+const { DATABASE_URL, DATABASE_AUTH_TOKEN } = process.env;
+
+if (!DATABASE_URL) {
+  throw new Error("Missing env: DATABASE_URL");
+}
+
+if (!DATABASE_AUTH_TOKEN) {
+  throw new Error("Missing env: DATABASE_AUTH_TOKEN");
+}
 
 const db = drizzle({
   connection: {
-    url: process.env.DATABASE_URL,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: DATABASE_URL,
+    authToken: DATABASE_AUTH_TOKEN,
   },
 });
 
